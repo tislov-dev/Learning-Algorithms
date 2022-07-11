38c8f5f8501b834e06d67f37d12802a494181fbc
tislov-dev <75918631+tislov-dev@users.noreply.github.com>
2022-07-11 17:54:38 +0000

    refractor

---
+++ LinkedList.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+def print_list(self):
+    temp = self.head
+    while temp is not None:
+        print(temp.value)
+        temp = temp.next
